562d9f1add13c62600c910e451bffe77e6241bbf
Denis Zverev <zverev.denisa7@yandex.ru>
2024-05-19 13:32:30 +0300

    add Delete and Update method

--- maps/map_test.go
+++ maps/map_test.go
@@ -13,12 +13,9 @@ func TestSearch(t *testing.T) {
 		assertStrings(t, got, want)
 	})
 	t.Run("unknown word", func(t *testing.T) {
-		want := "unknown word"
 		_, err := dictionary.Search("unknown")
-		if err == nil {
-			t.Fatalf("expected error but got %v", err)
-		}
-		assertStrings(t, err.Error(), want)
+
+		assertError(t, err, ErrNotFound)
 
 	})
 }
@@ -29,5 +26,84 @@ func assertStrings(t testing.TB, got, want string) {
 	if got != want {
 		t.Errorf("want: %s got: %s", want, got)
 	}
+
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("want: %s got: %s", want, got)
+	}
+
+}
+
+func TestAdd(t *testing.T) {
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		word := "dance"
+		definition := "is happy moving"
+
+		err := dictionary.Add(word, definition)
+
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, word, definition)
+	})
+
+	t.Run("existing word", func(t *testing.T) {
+		word := "dance"
+		definition := "is happy moving"
+		dictionary := Dictionary{word: definition}
+
+		err := dictionary.Add(word, "alala")
+
+		assertError(t, err, ErrWordAlreadyExist)
+		assertDefinition(t, dictionary, word, definition)
+
+	})
+}
+
+func assertDefinition(t *testing.T, dict Dictionary, word, def string) {
+	t.Helper()
+	got, err := dict.Search(word)
+	if err != nil {
+		t.Fatalf("error of getting word: %v", err)
+	}
+	assertStrings(t, got, def)
+}
+
+func TestUpdate(t *testing.T) {
+	t.Run("word exist", func(t *testing.T) {
+		word := "dance"
+		definition := "is happy moving"
+		dictionary := Dictionary{word: definition}
+
+		newDefinition := "a la la ho"
+
+		err := dictionary.Update(word, newDefinition)
+		assertError(t, nil, err)
+		assertDefinition(t, dictionary, word, newDefinition)
+	})
+	t.Run("word not exist", func(t *testing.T) {
+		word := "dance"
+		dictionary := Dictionary{}
+		newDefinition := "a la la ho"
+
+		err := dictionary.Update(word, newDefinition)
+
+		assertError(t, err, ErrWordNotExist)
+
+	})
+}
+
+func TestDelete(t *testing.T) {
+	word := "dance"
+	definition := "is happy moving"
+	dictionary := Dictionary{word: definition}
+
+	dictionary.Delete(word)
+
+	_, err := dictionary.Search(word)
+	assertError(t, err, ErrNotFound)
 
 }
--- maps/maps.go
+++ maps/maps.go
@@ -1,13 +1,50 @@
 package maps
 
-import "errors"
-
 type Dictionary map[string]string
+
+const (
+	ErrNotFound         = DictionaryErr("unknown word")
+	ErrWordAlreadyExist = DictionaryErr("word already exist")
+	ErrWordNotExist     = DictionaryErr("word does not exist")
+)
+
+type DictionaryErr string
+
+func (d DictionaryErr) Error() string {
+	return string(d)
+}
 
 func (d Dictionary) Search(key string) (string, error) {
 	v, ok := d[key]
 	if !ok {
-		return "", errors.New("unknown word")
+		return "", ErrNotFound
 	}
 	return v, nil
+}
+
+func (d Dictionary) Add(key, value string) error {
+	if _, ok := d[key]; !ok {
+		d[key] = value
+		return nil
+	} else {
+		return ErrWordAlreadyExist
+	}
+
+}
+
+func (d Dictionary) Update(key, value string) error {
+	_, ok := d[key]
+
+	switch ok {
+	case true:
+		d[key] = value
+		return nil
+	case false:
+		return ErrWordNotExist
+	}
+	return nil
+}
+
+func (d Dictionary) Delete(key string) {
+	delete(d, key)
 }
