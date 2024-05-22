d3912773e0ef350b05f082b676becf7c4da93673
Denis Zverev <zverev.denisa7@yandex.ru>
2024-05-22 21:29:50 +0300

    httptest_server

--- selectconcur/select_concur.go
+++ selectconcur/select_concur.go
@@ -1,18 +1,35 @@
 package selectconcur
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 )
 
-func Racer(a, b string) string {
-	aDuration := measureResponseTime(a)
-	bDuration := measureResponseTime(b)
+const tenSecondTimeout = time.Second * 10
 
-	if aDuration > bDuration {
-		return b
+func Racer(a, b string) (string, error) {
+	return ConfigurableRacer(a, b, tenSecondTimeout)
+}
+
+func ConfigurableRacer(a, b string, timeout time.Duration) (string, error) {
+	select {
+	case <-ping(a):
+		return a, nil
+	case <-ping(b):
+		return b, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("error of waiting response more than 10 second for %s and %s", a, b)
 	}
-	return a
+}
+
+func ping(s string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(s)
+		close(ch)
+	}()
+	return ch
 }
 
 func measureResponseTime(a string) time.Duration {
--- selectconcur/select_test.go
+++ selectconcur/select_test.go
@@ -8,22 +8,39 @@ import (
 )
 
 func TestRace(t *testing.T) {
-	slowServer := makeDelayedServer(20 * time.Millisecond)
-	fastServer := makeDelayedServer(0 * time.Millisecond)
+	t.Run("chech wich URL faster", func(t *testing.T) {
+		slowServer := makeDelayedServer(20 * time.Millisecond)
+		fastServer := makeDelayedServer(0 * time.Millisecond)
 
-	defer slowServer.Close()
-	defer fastServer.Close()
+		defer slowServer.Close()
+		defer fastServer.Close()
 
-	slowURL := slowServer.URL
-	fastURL := fastServer.URL
-	t.Log(slowURL, fastURL)
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
 
-	want := fastURL
-	got := Racer(slowURL, fastURL)
+		want := fastURL
+		got, err := Racer(slowURL, fastURL)
+		if err != nil {
+			t.Fatalf("too lon waiting respone from servers %v", err)
+		}
 
-	if got != want {
-		t.Errorf("got %q,want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q,want %q", got, want)
+		}
+	})
+
+	t.Run("return error if server response more than specified time", func(t *testing.T) {
+		server := makeDelayedServer(25 * time.Millisecond)
+
+		defer server.Close()
+
+		_, err := ConfigurableRacer(server.URL, server.URL, time.Millisecond*20)
+
+		if err == nil {
+			t.Error("expected error but got nil")
+		}
+
+	})
 }
 
 func makeDelayedServer(delay time.Duration) *httptest.Server {
